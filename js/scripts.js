fac87409859513886a0b55e88280f72b4e37ee52
Niem Nguyen and Ryan Carlile <ryan@>
2016-03-21 13:13:34 -0700

    This will create a branch for the measurement JavaScript app

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-var add = function(number1, number2) {
-	return number1 +number2;
+//Gallons to liters gal/0.26417
+//tablespoon to ml = tablespoon = 14.787ml
+//liters to oz	= 1lt = 33.814oz
+var measure = function(){
+var num1= parseInt(prompt("Enter number of gallons: "));
+var num2= parseInt(prompt("Enter number of tablespoons: "));
+var num3= parseInt(prompt("Enter number of liters: "));
+var gallon= num1 / 0.26417;
+var tablespoon = num2 * 14.787;
+var liters = num3 * 33.814;
+alert("Your conversions are " + gallon + "liters, " + tablespoon + "ml, and " + liters + "oz" );
 };
 
-alert(add(10,5));
-
-var number1 = parseInt(prompt("Enter a Magic Number: "));
-
-var number2 = parseInt(prompt("Enter another Magic Number: "));
-
-var result = add(number1, number2);
-alert(result);
+measure ();
